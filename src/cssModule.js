7dbe5eec87a485cd13c4c0d7973fbe40148d15f4
yuanhaokai <yuanhaokai@meituan.com>
2022-07-26 21:25:50 +0800

    css Module

---
+++ src/cssModule.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './test.module.css'
+export default function cssModule() {
+  return (
+    <div >
+        <button className={styles.test}>1</button>
+        <button className="test">2</button>
+        redux
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Appp from './ reverseProxy';
-import reportWebVitals from './reportWebVitals';
+import Appp from './cssModule';
+import {Provider} from 'react-redux';
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Appp />
-  </React.StrictMode>
 );
-
-reportWebVitals();
